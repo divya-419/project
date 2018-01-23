2463ff64989007f0fa32c173ce15f0a58f876dd4
divya-419 <35718424+divya-419@users.noreply.github.com>
2018-01-23 14:21:13 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,16 @@
+
+
+<?php
+ $conn = mysqli_connect('localhost','root','','bank');
+ if($conn == false)
+	 
+ 
+ {
+	// echo "Connection is not established";
+ }
+ else
+ {
+	// echo "Connection is successful:";
+	 
+ }
+ ?>
